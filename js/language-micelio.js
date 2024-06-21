c867cdf63b3d9ffbd0cf299d915b5e7656ebaa19
martin-centurion <martinalejandrocenturion@gmail.com>
2024-06-21 09:25:00 -0300

    agg micelio / act pipa

--- js/language-micelio.js
+++ js/language-micelio.js
@@ -16,32 +16,32 @@ const navbar = {
 const micelio = {
     es: {
         titleMicelio: "Sitio Web",
-        descMicelio: "Próximamente",
+        descMicelio: "Micelio Marketing Digital | Agencia",
         introduccionMicelio: "Introducción",
-        textMicelio: "Proximamente",
+        textMicelio: "Desarrollamos un sitio web moderno y funcional que refleja la identidad de la agencia y sus servicios. El objetivo principal fue crear una plataforma intuitiva y visualmente atractiva que mejore la experiencia del usuario y aumente las conversiones.",
         clienteMicelio: "Cliente",
         fechaMicelio: "Fecha",
-        fechaMicelio2: "Próximamente",
+        fechaMicelio2: "Junio 2024",
         categoriaMicelio: "Categoría",
         categoriaMicelio2: "SITIO WEB, REACT JS & JAVASCRIPT",
         tagsMicelio: "Diseño, Minimalista, Moderno",
         herramientasMicelio: "Herramientas.",
-        herramientasTextMicelio: "Próximamente",
+        herramientasTextMicelio: "Para este proyecto, utilizamos Figma para el diseño y WordPress para el desarrollo, asegurando un rendimiento óptimo y una experiencia de usuario fluida. También implementamos herramientas de SEO para mejorar la visibilidad del sitio en los motores de búsqueda.",
         siguienteProyecto: "Siguiente Proyecto"
     },
     en: {
         titleMicelio: "Website",
-        descMicelio: "Coming Soon",
+        descMicelio: "Micelio Marketing Digital | Agency",
         introduccionMicelio: "Introduction",
-        textMicelio: "Coming Soon",
+        textMicelio: "We develop a modern and functional website that reflects the identity of the agency and its services. The main goal was to create an intuitive and visually appealing platform that improves user experience and increases conversions.",
         clienteMicelio: "Client",
         fechaMicelio: "Date",
-        fechaMicelio2: "Coming Soon",
+        fechaMicelio2: "June 2024",
         categoriaMicelio: "Category",
         categoriaMicelio2: "WEB DESIGN, REACT JS & JAVASCRIPT",
         tagsMicelio: "Design, Minimalist, Modern",
         herramientasMicelio: "Tools.",
-        herramientasTextMicelio: "Coming Soon",
+        herramientasTextMicelio: "For this project, we used Figma for design and WordPress for development, ensuring optimal performance and a smooth user experience. We also implement SEO tools to improve the site's visibility in search engines.",
         siguienteProyecto: "Next Project"
     }
 }
@@ -56,17 +56,17 @@ function changeLanguage(lang) {
     document.getElementById('in').innerText = navbar[lang].in;
     document.getElementById('titleMicelio').innerText = micelio[lang].titleMicelio;
     document.getElementById('descMicelio').innerText = micelio[lang].descMicelio;
-    /* document.getElementById('introduccionMicelio').innerText = Micelio[lang].introduccionMicelio;
-    document.getElementById('textMicelio').innerText = Micelio[lang].textMicelio;
-    document.getElementById('clienteMicelio').innerText = Micelio[lang].clienteMicelio;
-    document.getElementById('fechaMicelio').innerText = Micelio[lang].fechaMicelio;
-    document.getElementById('fechaMicelio2').innerText = Micelio[lang].fechaMicelio2;
-    document.getElementById('categoriaMicelio').innerText = Micelio[lang].categoriaMicelio;
-    document.getElementById('categoriaMicelio2').innerText = Micelio[lang].categoriaMicelio2;
-    document.getElementById('tagsMicelio').innerText = Micelio[lang].tagsMicelio;
-    document.getElementById('herramientasMicelio').innerText = Micelio[lang].herramientasMicelio;
-    document.getElementById('herramientasTextMicelio').innerText = Micelio[lang].herramientasTextMicelio; */
-    document.getElementById('siguienteProyecto').innerText = Micelio[lang].siguienteProyecto;
+    document.getElementById('introduccionMicelio').innerText = micelio[lang].introduccionMicelio;
+    document.getElementById('textMicelio').innerText = micelio[lang].textMicelio;
+    document.getElementById('clienteMicelio').innerText = micelio[lang].clienteMicelio;
+    document.getElementById('fechaMicelio').innerText = micelio[lang].fechaMicelio;
+    document.getElementById('fechaMicelio2').innerText = micelio[lang].fechaMicelio2;
+    document.getElementById('categoriaMicelio').innerText = micelio[lang].categoriaMicelio;
+    document.getElementById('categoriaMicelio2').innerText = micelio[lang].categoriaMicelio2;
+    document.getElementById('tagsMicelio').innerText = micelio[lang].tagsMicelio;
+    document.getElementById('herramientasMicelio').innerText = micelio[lang].herramientasMicelio;
+    document.getElementById('herramientasTextMicelio').innerText = micelio[lang].herramientasTextMicelio;
+    document.getElementById('siguienteProyecto').innerText = micelio[lang].siguienteProyecto;
 }
 
 // Inicializa el contenido con la preferencia del usuario o en español por defecto
